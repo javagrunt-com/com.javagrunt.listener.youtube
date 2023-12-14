48874104fbe23bd84c52d6c7f4e68cef6ca8d7aa
dashaun <dashaun@dashaun.com>
2023-12-14 09:18:38 -0600

    initialized

---
+++ src/main/java/com/javagrunt/listener/youtube/Application.java
@@ -0,0 +1,28 @@
+package com.javagrunt.listener.youtube;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+@RestController
+class YouTubeController {
+
+	Logger logger = LoggerFactory.getLogger(YouTubeController.class);
+
+	@PostMapping("/")
+	String subscriber(@RequestBody String atomXml) {
+		logger.info("Received: " + atomXml);
+		return "OK";
+	}	
+}
---
+++ src/test/java/com/javagrunt/listener/youtube/TestApplication.java
@@ -0,0 +1,24 @@
+package com.javagrunt.listener.youtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestApplication {
+
+    @Bean
+    @ServiceConnection(name = "openzipkin/zipkin")
+    GenericContainer<?> zipkinContainer() {
+        return new GenericContainer<>(DockerImageName.parse("openzipkin/zipkin:latest")).withExposedPorts(9411);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(Application::main).with(TestApplication.class).run(args);
+    }
+
+}
+
